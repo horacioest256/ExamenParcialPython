c0d7ed9192c0d886d2d179f3e3504b75b40c830b
horacioest256 <horacioest@gmail.com>
2024-08-18 23:28:27 -0500

    pregunta 2

--- wikiApp/models.py
+++ wikiApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class tema(models.Model):
+    nombre = models.CharField(max_length=128, null=True, blank=True)
+    descripcion = models.CharField(max_length=256, null=True, blank=True)
+
+
+class articulo(models.Model):
+    titulo = models.CharField(max_length=128, null=True, blank=True)
+    contenido = models.CharField(max_length=1024, null=True, blank=True)
+    temaRelacionado = models.ForeignKey(tema, null=True, blank=True, on_delete=models.SET_NULL)
